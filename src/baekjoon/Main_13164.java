4a5226e2041d2b3be5f06fae3de6f33fdabdd55a
권용환 <hahahaa_a@naver.com>
2022-01-20 13:29:49 +0900

    first commit

---
+++ src/baekjoon/Main_13164.java
@@ -0,0 +1,43 @@
+/**
+ * 백준 13164번
+ * 행복 유치원
+ */
+
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main_13164 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] children = new int[n];
+        for (int i = 0; i < n; i++) {
+            children[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] diff = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = children[i + 1] - children[i];
+        }
+
+        Arrays.sort(diff);
+
+        int cost = 0;
+        for (int i = 0; i < n - k; i++) {
+            cost += diff[i];
+        }
+
+        System.out.println(cost);
+    }
+}
