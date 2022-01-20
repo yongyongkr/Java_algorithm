888f0084259e476ff008ea4b02f1206962f3e7a3
권용환 <hahahaa_a@naver.com>
2022-01-21 00:13:27 +0900

    Main_1922 solved

---
+++ src/baekjoon/Main_1922.java
@@ -0,0 +1,24 @@
+/**
+ * 백준 1922번
+ * 네트워크 연결
+ * https://www.acmicpc.net/problem/1922
+ */
+
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1922 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		int[][] matrix = new int[m + 1][m + 1];
+		boolean[] visited = new boolean[m + 1];
+
+	}
+}
