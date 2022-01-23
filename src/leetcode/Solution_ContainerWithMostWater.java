df823247bb772e03bdc24635ee5d499fdda389f2
권용환 <hahahaa_a@naver.com>
2022-01-23 20:03:53 +0900

    Solution_ContainerWithMostWater

---
+++ src/leetcode/Solution_ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+/**
+ * leetcode
+ * 11. Container With Most Water
+ * https://leetcode.com/problems/container-with-most-water/
+ */
+
+package leetcode;
+
+public class Solution_ContainerWithMostWater {
+	public int maxArea(int[] height) {
+		int max = 0;
+		int left = 0;
+		int right = height.length - 1;
+
+		while (left < right) {
+			if (height[left] < height[right]) {
+				max = Math.max(max, height[left] * (right - left));
+				left++;
+			} else {
+				max = Math.max(max, height[right] * (right - left));
+				right--;
+			}
+		}
+		return max;
+	}
+}
