532f4d2feabfef1056fdb63e22da508ea34a6121
권용환 <hahahaa_a@naver.com>
2022-01-28 21:45:59 +0900

    Solution_SortColors

---
+++ src/leetcode/Solution_SortColors.java
@@ -0,0 +1,16 @@
+/**
+ * leetcode
+ * 75. Sort Colors
+ * https://leetcode.com/problems/sort-colors/
+ */
+
+package leetcode;
+
+import java.util.Arrays;
+
+public class Solution_SortColors {
+	public void sortColors(int[] nums) {
+		Arrays.sort(nums);
+		System.out.println(Arrays.toString(nums));
+	}
+}
