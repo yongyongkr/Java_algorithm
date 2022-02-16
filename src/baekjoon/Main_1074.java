4c7e8c6e417d6e8799787ce5d0a112f1c060f98f
권용환 <hahahaa_a@naver.com>
2022-02-16 09:06:43 +0900

    Main_1074

--- src/baekjoon/Main_1074.java
+++ src/baekjoon/Main_1074.java
@@ -1,2 +1,62 @@
-package baekjoon;public class Main_1074 {
+/**
+ * 백준 1074번
+ * Z
+ * https://www.acmicpc.net/problem/1074
+ */
+
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1074 {
+
+	static int r, c;
+	static long cnt;
+	static boolean flag;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+
+		int n = (int)Math.pow(2, N);
+		cnt = - 1;
+
+		divideConquer(n, 0, 0);
+		System.out.println(cnt);
+	}
+
+	private static void divideConquer(int x, int sr, int sc) {
+		if (flag) return;
+
+		if (sr == r && sc == c) {
+			cnt++;
+			flag = true;
+			return;
+		}
+
+		if (x == 1) {
+			cnt++;
+			return;
+		}
+
+		if (r < sr + x / 2 && c < sc + x / 2) {
+			divideConquer(x / 2, sr, sc);
+		} else if (r < sr + x / 2 && sc + x / 2 <= c && c < sc + x) {
+			cnt += (long)x * x / 4;
+			divideConquer(x / 2, sr, sc + x / 2);
+		} else if (sr + x / 2 <= r && r < sr + x && c < sc + x / 2) {
+			cnt += (long)x * x / 2;
+			divideConquer(x / 2, sr + x / 2, sc);
+		} else {
+			cnt += (long)x * x * 3 / 4;
+			divideConquer(x / 2, sr + x / 2, sc + x / 2);
+		}
+	}
 }
