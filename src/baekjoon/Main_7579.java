81249407c4bc30a0f6ea97b76a1602274f2a81ae
권용환 <hahahaa_a@naver.com>
2022-03-01 14:49:42 +0900

    Main_7579

--- src/baekjoon/Main_7579.java
+++ src/baekjoon/Main_7579.java
@@ -1,2 +1,61 @@
-package baekjoon;public class Main_7579 {
+/**
+ * 백준 7579번
+ * 앱
+ * https://www.acmicpc.net/problem/7579
+ */
+
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_7579 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int ans = Integer.MAX_VALUE;
+
+		int[] memoryArr = new int[n];
+		int[] costArr = new int[m];
+		int[][] dp = new int[n][100001];
+
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			memoryArr[i] = Integer.parseInt(st1.nextToken());
+			costArr[i] = Integer.parseInt(st2.nextToken());
+		}
+
+		for (int i = 0; i < n; i++) {
+			int cost = costArr[i];
+			int memory = memoryArr[i];
+
+			for (int j = 0; j <= 10000; j++) {
+				if (i == 0) {
+					if (j >= cost) {
+						dp[i][j] = memory;
+					}
+				} else {
+					if (j >= cost) {
+						dp[i][j] = Math.max(dp[i - 1][j - cost] + memory, dp[i - 1][j]);
+					} else {
+						dp[i][j] = dp[i - 1][j];
+					}
+				}
+
+				if (dp[i][j] >= m) {
+					ans = Math.min(ans, j);
+				}
+			}
+		}
+
+		System.out.println(ans);
+	}
 }
