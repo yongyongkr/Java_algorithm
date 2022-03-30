103b8c89e9d5c9d862dfb95eaf79b494b998c639
권용환 <hahahaa_a@naver.com>
2022-03-30 18:10:16 +0900

    Main_1949

--- src/baekjoon/Main_1949.java
+++ src/baekjoon/Main_1949.java
@@ -1,2 +1,42 @@
-package baekjoon;public class Main_1949 {
+/**
+ * 백준 1949번
+ * 우수 마을
+ * https://www.acmicpc.net/problem/1949
+ */
+
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main_1949 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] towns = new int[n + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+			towns[i] = Integer.parseInt(st.nextToken());
+		}
+
+		List<List<Integer>> list = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			list.add(new ArrayList<>());
+		}
+		for (int i = 0; i < n - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int node1 = Integer.parseInt(st.nextToken());
+			int node2 = Integer.parseInt(st.nextToken());
+			list.get(node1).add(node2);
+			list.get(node2).add(node1);
+		}
+
+
+	}
 }
